41ecb8749a51abd7e37a65ae05a8a0ec3bf57a5e
Gev1724 <gev.gabrielyan1724@gmail.com>
2018-08-15 16:47:28 +0400

    created drop down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { NzMessageService, UploadFile } from 'ng-zorro-antd';
+import * as FA from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  imgUrl = '';
+  showImg = false;
+  savedImg = '';
+  file = '';
+  titleOn = true;
+  Fa = FA;
+  title = '';
+  text = '';
+  fileList = [
+  ];
+  previewImage = '';
+  postArray = [];
+
+  constructor(private msg: NzMessageService) {}
+  ngOnInit() {
+  }
+  onClick(event) {
+    console.log(event);
+    if (event.path[0].className === 'ant-upload-list-item-info') {
+      this.file = event.path[0].children[0].children[0].children[0].src;
+    }
+  }
+  titleOnF() {
+    this.titleOn = true;
+  }
+  titleOffF() {
+    this.titleOn = false;
+  }
+  addImage() {
+    if (this.file !== '') {
+      this.savedImg = this.file;
+      this.postArray.push(this.savedImg);
+      this.file = '';
+    }
+  }
+  addImgUrl() {
+    if (this.imgUrl !== '') {
+      this.savedImg = this.imgUrl;
+      this.postArray.push(this.savedImg);
+      this.imgUrl = '';
+      this.showImg = false;
+    }
+  }
+  back() {
+    this.file = '';
+  }
+  showImgF() {
+    this.showImg = true;
+  }
+  // onEvent(event) {
+  //   if (this.imgUrl) {
+  //     document.getElementById('hover').style.left = event.offsetX + 'px';
+  //   }
+  // }
+  handlePreview = (file: UploadFile) => {
+    this.previewImage = file.url || file.thumbUrl;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import {NZ_I18N, en_US, NgZorroAntdModule} from 'ng-zorro-antd';
+import en from '@angular/common/locales/en';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
+registerLocaleData(en);
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
